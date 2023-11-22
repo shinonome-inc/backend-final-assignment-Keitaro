87b4ee4156833ec7d289e50e886491f2e6fd3e15
KeaitaroYabu <y2k.prog@gmail.com>
2023-11-23 02:28:45 +0900

    ツイート詳細機能と削除機能

--- tweets/tests.py
+++ tweets/tests.py
@@ -68,16 +68,51 @@ class TestTweetCreateView(TestCase):
         self.assertIn(f"この値は {TWEET_MAX_LENGTH} 文字以下でなければなりません( {TWEET_LENGTH} 文字になっています)。", form.errors["content"])
 
 
-# class TestTweetDetailView(TestCase):
-#     def test_success_get(self):
+class TestTweetDetailView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="testpassword")
+        self.client.force_login(self.user)
+        self.tweet = Tweet.objects.create(content="test", user=self.user)
+
+    def test_success_get(self):
+        response = self.client.get(reverse("tweets:detail", kwargs={"pk": self.tweet.id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tweets/detail.html")
+        self.assertEqual(response.context["object"], self.tweet)
 
 
-# class TestTweetDeleteView(TestCase):
-#     def test_success_post(self):
+class TestTweetDeleteView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="testpassword")
+        self.another_user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client.force_login(self.user)
+        self.tweet = Tweet.objects.create(content="test", user=self.user)
+        self.another_tweet = Tweet.objects.create(content="testuser", user=self.another_user)
 
-#     def test_failure_post_with_not_exist_tweet(self):
+    def test_success_post(self):
+        response = self.client.post(reverse("tweets:delete", kwargs={"pk": self.tweet.id}))
 
-#     def test_failure_post_with_incorrect_user(self):
+        self.assertRedirects(
+            response,
+            reverse("tweets:home"),
+            status_code=302,
+            target_status_code=200,
+        )
+        self.assertFalse(Tweet.objects.filter(content=self.tweet.content).exists())
+
+    def test_failure_post_with_not_exist_tweet(self):
+        self.client.post(reverse("tweets:delete", kwargs={"pk": self.tweet.id}))
+        response = self.client.post(reverse("tweets:delete", kwargs={"pk": self.tweet.id}))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(Tweet.objects.filter(content=self.another_tweet.content).exists())
+
+    def test_failure_post_with_incorrect_user(self):
+        response = self.client.post(reverse("tweets:delete", kwargs={"pk": self.another_tweet.id}))
+
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(Tweet.objects.filter(content=self.another_tweet.content).exists())
 
 
 # class TestLikeView(TestCase):
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
 
 from .forms import CreateTweetForm
 from .models import Tweet
@@ -19,3 +21,22 @@ class TweetCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class TweetDetailView(LoginRequiredMixin, DetailView):
+    template_name = "tweets/detail.html"
+    model = Tweet
+
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "tweets/delete.html"
+    model = Tweet
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        if not self.get_object():
+            raise Http404
+        elif self.request.user != self.get_object().user:
+            raise PermissionDenied
+        else:
+            return super().form_valid(form)
